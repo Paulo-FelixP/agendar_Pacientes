cfcff577a38f81abecedfb5d028950cc7c498177
Paulo Felix <Paulo.felixper@gmail.com>
2026-06-27 19:30:31 -0300

    Primeiro Commit

---
+++ src/main/java/com/clinica/agendar/AgendarApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.agendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendarApplication.class, args);
+	}
+
+}
